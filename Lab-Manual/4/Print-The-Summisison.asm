5c7cbf334c7513bdd27a98d186abb8a0e35c9fa5
Md. Sohan Millat Sakib <millatsakib01@gmail.com>
2025-12-10 00:59:35 +0600

    Summission of series number code add

--- Lab-Manual/4/Print-The-Summisison.asm
+++ Lab-Manual/4/Print-The-Summisison.asm
@@ -1,13 +1,17 @@
 ; You may customize this and other start-up templates; 
 ; The location of this template is c:\emu8086\inc\0_com_template.txt
 
+
+; Print the summission of a seris number 
+
+
 org 100h
 
 .DATA
 N DB ?
-
+NEWLINE DB 0DH,0AH,"$"
 MSG1 DB "Enter The range of summition: $"
-MSG2 DB "The summission of the range is: $" 
+MSG2 DB " The summission of the range is: $" 
 
 
 .CODE
@@ -23,19 +27,48 @@ MAIN PROC
     INT 21H 
     SUB AL,48
     MOV N,AL
-    MOV BL,0
     
+    ; Reset BL and CX register
+    MOV BL,0
     MOV CX,0
+    
+    
     MOV CL, AL
     
     START: ADD BL, CL   ; LOOPING START
     LOOP START ; CL DECREMENTS AUTOMATICALLY
     
+    ;PRINT THE NEW LINE
+    MOV AH,09H
+    LEA DX,NEWLINE
+    INT 21H 
+    
+    
+    ;PRINGTHE MESSAGE 2
+    MOV AH,09H
+    LEA DX,MSG2
+    INT 21H
+    
     MOV AX,0
     MOV AL,BL
-    ADD AL,48
-    MOV AH, 02H
-    INT 21H   
+    MOV BL,10
+    DIV BL
+    MOV BL,AL
+    MOV BH,AH
+    
+    
+    ;PRINT THE NUMEBR
+    
+    ADD BL,48
+    MOV AH, 02H  
+    MOV DL,BL
+    INT 21H  
+    
+    
+    ADD BH,48
+    MOV AH, 02H  
+    MOV DL,BH
+    INT 21H  
     
     
     MAIN ENDP
