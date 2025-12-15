d334cc639657e2c01370d9394f61d7dcd086d447
Md. Sohan Millat Sakib <millatsakib01@gmail.com>
2025-12-15 17:16:05 +0600

    Two number add code add

---
+++ Lab-Manual/7/1.Add-Two-Number.asm
@@ -0,0 +1,24 @@
+; Macro for add two number 
+
+ADDITION MACRO NUM1, NUM2
+    
+    MOV AX, NUM1
+    MOV BX, NUM2
+    
+    ADD AX, BX
+ENDM
+
+ORG 100H
+
+.DATA
+    A DW 10
+    B DW 5
+    
+.CODE
+    MAIN PROC
+        ADDITION A,B    ; CALLING MACRO
+        
+        MOV AH,4CH
+        INT 21H
+    MAIN ENDP
+END MAIN
